2ce62ad44ccb65e12fc50f6255001d7316b81bfb
DreamPrism <thelastprism@qq.com>
2023-03-05 19:57:19 +0800

    removed unnecessary things

--- app.py
+++ app.py
@@ -30,24 +30,19 @@ def inference(img, mode):
     if mode == "base":
         run_cmd("python inference_realesrgan.py -n RealESRGAN_x4plus -i " + INPUT_DIR + " -o " + OUTPUT_DIR)
     else:
-        os.system("python inference_realesrgan.py -n RealESRGAN_x4plus_anime_6B -i " + INPUT_DIR + " -o " + OUTPUT_DIR)
+        run_cmd("python inference_realesrgan.py -n RealESRGAN_x4plus_anime_6B -i " + INPUT_DIR + " -o " + OUTPUT_DIR)
     return os.path.join(OUTPUT_DIR, "1_out.png")
 
 
 title = "Real-ESRGAN"
-description = "Real-ESRGAN超分辨率模型的Gradio Demo 一次请提交一张图片\n动漫插图等图片请选择anime\n其他图片请选择base"
+description = "Real-ESRGAN超分辨率模型的Gradio Demo 一次请提交一张图片\r\n动漫插图等图片请选择anime模式\r\n其他图片请选择base模式"
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2107.10833'>Real-ESRGAN: Training Real-World Blind Super-Resolution with Pure Synthetic Data</a> | <a href='https://github.com/xinntao/Real-ESRGAN'>Github Repo</a></p>"
 
 gr.Interface(
     inference,
     [gr.inputs.Image(type="pil", label="Input"), gr.inputs.Radio(
-        ["base", "anime"], type="value", default="base", label="model type")],
+        ["base", "anime"], type="value", default="anime", label="模式")],
     gr.outputs.Image(type="file", label="Output"),
     title=title,
-    share=True,
     description=description,
-    article=article,
-    examples=[
-        ['bear.jpg', 'base'],
-        ['anime.png', 'anime']
-    ]).launch(share=True)
+    article=article).launch(share=True)
